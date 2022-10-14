f8e4e85dc32a27e48f13fc48878fe36628920a99
NilEis <52820950+NilEis@users.noreply.github.com>
2022-10-14 16:51:34 +0200

    added module and git support

--- include/templates.h
+++ include/templates.h
@@ -1,7 +1,7 @@
 #ifndef TEMPLATES_H
 #define TEMPLATES_H
 
-#define CMAKELISTS_TXT "cmake_minimum_required(VERSION 3.9.0)\nproject(%s VERSION 0.1.0 LANGUAGES %s)\n\nif (CMAKE_C_COMPILER_ID STREQUAL \"GNU\")\n    add_compile_options(-ffunction-sections -fdata-sections)\nendif()\n\n%s(%s ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c)\n\ntarget_include_directories(%s PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/include/)\n\ninclude(CheckIPOSupported)\n\nif (CMAKE_C_COMPILER_ID STREQUAL \"GNU\")\n    target_link_options(%s PRIVATE -Wl,--gc-sections)\nendif()\n\ncheck_ipo_supported(RESULT result OUTPUT output LANGUAGES C)\nif(result)\nmessage(STATUS \"IPO is supported\")\nset_property(TARGET %s PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)\nelse()\nmessage(WARNING \"IPO is not supported: ${output}\")\nendif()\n"
+#define CMAKELISTS_TXT "cmake_minimum_required(VERSION 3.9.0)\nproject(%s VERSION 0.1.0 LANGUAGES %s)\n\nif (CMAKE_C_COMPILER_ID STREQUAL \"GNU\")\n    add_compile_options(-ffunction-sections -fdata-sections)\nendif()\n\n%s(%s ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c)\n\ntarget_include_directories(%s PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/include/)\n\n%s\n\ninclude(CheckIPOSupported)\n\nif (CMAKE_C_COMPILER_ID STREQUAL \"GNU\")\n    target_link_options(%s PRIVATE -Wl,--gc-sections)\nendif()\n\ncheck_ipo_supported(RESULT result OUTPUT output LANGUAGES C)\nif(result)\nmessage(STATUS \"IPO is supported\")\nset_property(TARGET %s PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)\nelse()\nmessage(WARNING \"IPO is not supported: ${output}\")\nendif()\n"
 const char cmakelists_txt_template[] = CMAKELISTS_TXT;
 #undef CMAKELISTS_TXT
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "terminal.h"
 #include "c_logger.h"
 #include "clib.h"
 #include "parse_args.h"
@@ -8,10 +9,13 @@
 #define EXAMPLE_NAME_DEF "Example_project"
 const static char *EXAMPLE_NAME = EXAMPLE_NAME_DEF;
 
+static void init_github(void);
+
 int main(int argc, char **argv)
 {
     args_t args = {EXAMPLE_NAME_DEF, "C", 0, 0};
     char *project_type = "add_executable";
+    char modules[4096] = {0};
     get_options(&argc, &argv, &args);
     if (args.name_set == 0)
     {
@@ -21,16 +25,6 @@ int main(int argc, char **argv)
         {
             clib_strcpy((char *)&args.project_name, EXAMPLE_NAME, 64);
         }
-    }
-    {
-        FILE *cmakelists = fopen("./CMakeLists.txt", "w");
-        if (args.lib)
-        {
-            project_type = "add_library";
-        }
-        fprintf(cmakelists, cmakelists_txt_template, args.project_name, args.project_language, project_type, args.project_name, args.project_name, args.project_name, args.project_name);
-        fclose(cmakelists);
-        c_logger_log(C_LOGGER_VERBOSE, "Created CMakeLists.txt\n");
     }
     {
         FILE *gitignore = fopen("./.gitignore", "w");
@@ -47,16 +41,74 @@ int main(int argc, char **argv)
     }
     if (args.git)
     {
-        if (system("git --version") == 0)
+        init_github();
+    }
+    else
+    {
+        terminal_clear();
+        printf("Initialise GitHub [Y/n]: ");
+        int ch = clib_getch();
+        if (ch == 'Y' || ch == 'y' || ch != 'N' || ch != 'n')
         {
-
-            system("git init && git add . && git commit -m \"Initial commit\"");
-            c_logger_log(C_LOGGER_VERBOSE, "Initialised git\n");
-        }
-        else
-        {
-            c_logger_log(C_LOGGER_ERROR, "git needs to be installed for --git to work (If git is installed please open an issue on github)\n");
+            args.git = 1;
+            init_github();
         }
     }
+    if (args.git == 1)
+    {
+        char str[2048] = {0};
+        printf("Enter external packages in the following format 'name,github_link;name,github_link;...':");
+        scanf("%s", &str);
+        size_t str_l = strlen(str);
+        if (str_l >= 3)
+        {
+            char *entry = strtok(str, ";");
+            while (entry != NULL)
+            {
+                char *name = NULL;
+                char *link = NULL;
+                link = strchr(entry, ',');
+                link[0] = '\0';
+                link++;
+                name = entry;
+                sprintf(modules, "%sadd_subdirectory(extern/%s)\n", modules, name);
+                if (name == NULL || name == NULL)
+                {
+                    c_logger_log(C_LOGGER_ERROR, "'%s,%s' is not a valid combination\n", name, link);
+                    break;
+                }
+                char *add_link = (char *)calloc(strlen(name) + strlen(link) + strlen("git submodule add l extern/n\0"), sizeof(char));
+                sprintf(add_link, "git submodule add %s extern/%s", link, name);
+                printf("name: %s\nlink: %s\n", name, link);
+                system(add_link);
+                free(add_link);
+                entry = strtok(NULL, ";");
+            }
+        }
+    }
+    {
+        FILE *cmakelists = fopen("./CMakeLists.txt", "w");
+        if (args.lib)
+        {
+            project_type = "add_library";
+        }
+        fprintf(cmakelists, cmakelists_txt_template, args.project_name, args.project_language, project_type, args.project_name, args.project_name, modules, args.project_name, args.project_name);
+        fclose(cmakelists);
+        c_logger_log(C_LOGGER_VERBOSE, "Created CMakeLists.txt\n");
+    }
     return 0;
+}
+
+void init_github(void)
+{
+    if (system("git --version") == 0)
+    {
+        system("git init && git add . && git commit -m \"Initial commit\"");
+        terminal_clear();
+        c_logger_log(C_LOGGER_VERBOSE, "Initialised git\n");
+    }
+    else
+    {
+        c_logger_log(C_LOGGER_ERROR, "git needs to be installed for --git to work (If git is installed please open an issue on github)\n");
+    }
 }
